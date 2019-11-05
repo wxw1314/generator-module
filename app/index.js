3db16f8aa9a5ebb3145dd101a79812e48a485b62
wxw1314 <1420925256@qq.com>
2019-11-05 15:07:31 +0800

    去掉eslint规则

--- app/index.js
+++ app/index.js
@@ -1,11 +1,11 @@
-let Generator  = require('yeoman-generator');
+let Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
   constructor(args, opts) {
     // 调用超级构造函数很重要，我们的生成器将被正确设置
     super(args, opts);
   }
-  prompting(){
+  prompting() {
     var questions = [
       {
         type: 'input',
@@ -19,52 +19,51 @@ module.exports = class extends Generator {
         message: '项目开发者',
         store: true,
         default: ''
-      },{
+      },
+      {
         type: 'input',
         name: 'projectVersion',
         message: '项目版本号',
         default: '0.0.1'
       }
-    ]
+    ];
     return this.prompt(questions).then(
-      function(answers){
-        for(var item in answers){
+      function(answers) {
+        for (var item in answers) {
           answers.hasOwnProperty(item) && (this[item] = answers[item]);
         }
-      }.bind(this));
+      }.bind(this)
+    );
   }
   initializing() {
-    this.log("开始构建项目...");
+    this.log('开始构建项目...');
   }
-  writing(){
+  writing() {
     //模板
+    this.fs.copy(this.templatePath('cmsModule/'), this.destinationPath(''));
     this.fs.copy(
-      this.templatePath('cmsModule/'),
-      this.destinationPath('')
+      this.templatePath('babelrc_tmpl'),
+      this.destinationPath('.babelrc')
+    );
+    // this.fs.copy(
+    //   this.templatePath("eslintrc_tmpl.js"),
+    //   this.destinationPath(".eslintrc.js")
+    // );
+    this.fs.copy(
+      this.templatePath('gitignore_tmpl'),
+      this.destinationPath('.gitignore')
     );
     this.fs.copy(
-      this.templatePath("babelrc_tmpl"),
-      this.destinationPath(".babelrc")
+      this.templatePath('npmrc_tmpl'),
+      this.destinationPath('.npmrc')
     );
     this.fs.copy(
-      this.templatePath("eslintrc_tmpl.js"),
-      this.destinationPath(".eslintrc.js")
+      this.templatePath('editorconfig_tmpl'),
+      this.destinationPath('.editorconfig')
     );
-    this.fs.copy(
-      this.templatePath("gitignore_tmpl"),
-      this.destinationPath(".gitignore")
-    );
-    this.fs.copy(
-      this.templatePath("npmrc_tmpl"),
-      this.destinationPath(".npmrc")
-    );
-    this.fs.copy(
-      this.templatePath("editorconfig_tmpl"),
-      this.destinationPath(".editorconfig")
-    );
-    this.fs.copy(
-      this.templatePath("eslintignore_tmpl"),
-      this.destinationPath(".eslintignore")
-    );
+    // this.fs.copy(
+    //   this.templatePath("eslintignore_tmpl"),
+    //   this.destinationPath(".eslintignore")
+    // );
   }
-}
+};
